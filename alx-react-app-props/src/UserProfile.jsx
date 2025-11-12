821be11f418458b6b373d12bd35758f4b614e72f
Tifloria1 <162379062+Tifloria1@users.noreply.github.com>
2025-11-12 10:38:44 +0100

    Refactor UserProfile component imports and styles
    Refactor UserProfile component to use named import for UserContext and adjust styles.

--- alx-react-app-props/src/UserProfile.jsx
+++ alx-react-app-props/src/UserProfile.jsx
@@ -1,12 +1,12 @@
 import { useContext } from 'react';
-import UserContext from './UserContext';
+import { UserContext } from './UserContext';
 
 function UserProfile() {
   const userData = useContext(UserContext);
 
   return (
-    <div style={{border:'1px solid #e5e7eb', padding:12, borderRadius:8}}>
-      <h3>User Profile (Context)</h3>
+    <div style={{ border: '1px solid #e5e7eb', padding: 12, borderRadius: 8 }}>
+      <h3 style={{ marginTop: 0 }}>User Profile (Context)</h3>
       <p>Name: {userData?.name}</p>
       <p>Email: {userData?.email}</p>
     </div>
